1e6c752a5adb92539360623a4d06252194e60d27
John <info@forumhulp.com>
2014-10-07 13:06:08 +0200

    Travis

--- language/en/info_ucp_selfdelete.php
+++ language/en/info_ucp_selfdelete.php
@@ -20,12 +20,12 @@ if (empty($lang) || !is_array($lang))
 $lang = array_merge($lang, array(
 	'UCP_SELFDELETE_TITLE'					=> 'Account deactiveren',
 	'UCP_PROFILE_DEACTIVATE'				=> 'Deactiveren',
-	'UCP_PROFILE_DEACTIVATE_USER'			=> 'Gebruiker deactiveren',	
+	'UCP_PROFILE_DEACTIVATE_USER'			=> 'Gebruiker deactiveren',
 	'UCP_PROFILE_DEACTIVATE_USER_EXPLAIN'	=> 'Hier kan je zelf jouw account deactiveren.<br />Het kan alleen door een administrator opgeheven worden.',
 	'UCP_PROFILE_DEACTIVATE_SURE'			=> 'Weet je zeker dat je jouw account wilt deactiveren?',
 	'UCP_PROFILE_DEACTIVATE_REASON'			=>	'Reden voor deactiveren',
 	'UCP_PROFILE_DEACTIVAT_CHAR'			=> '100 karakters max.',
-	
+
 	'UCP_PROFILE_DEACTIVAT_SUCCES'			=> 'Account gedeactiveerd!',
 	'UCP_PROFILE_DEACTIVAT_OWNER'			=> 'Eigenaars kunnen niet gedeactiveerd worden!',
 	'UCP_PROFILE_DEACTIVAT_FAIL'			=> 'Account niet gedeactiveerd<br />Paswoord niet opgegeven of klopt niet.'
--- ucp/selfdelete_module.php
+++ ucp/selfdelete_module.php
@@ -26,17 +26,17 @@ class selfdelete_module
 
 		$deactivate		= request_var('deactivate', '');
 		$cur_password	= request_var('cur_password', '');
-		$d_reason		= utf8_normalize_nfc(request_var('d_reason', 'Geen reden opgegeven!', true));	
+		$d_reason		= utf8_normalize_nfc(request_var('d_reason', 'Geen reden opgegeven!', true));
 		$uid 			= (int) $user->data['user_id'];
-		
+
 		$d_reason = substr($d_reason, 0, 100);
-		
+
 		if ($deactivate && !phpbb_check_hash($cur_password, $user->data['user_password']))
 		{
-			meta_refresh(5, append_sid($this->u_action));	
+			meta_refresh(5, append_sid($this->u_action));
 			trigger_error($user->lang['UCP_PROFILE_DEACTIVAT_FAIL'] . '<br /><br />' . sprintf($user->lang['RETURN_UCP'], '<a href="' . $this->u_action . '">', '</a>'));
 		}
-		if ($user->data['user_type'] == USER_FOUNDER) 
+		if ($user->data['user_type'] == USER_FOUNDER)
 		{
 			meta_refresh(5, append_sid("{$phpbb_root_path}ucp.$phpEx?i=profile"));
 			trigger_error($user->lang['UCP_PROFILE_DEACTIVAT_OWNER'] . '<br /><br />' . sprintf($user->lang['RETURN_UCP'], '<a href="' . $phpbb_root_path. 'ucp.' . $phpEx  .'?i=profile">', '</a>'));
@@ -47,11 +47,11 @@ class selfdelete_module
 			$sql = 'UPDATE ' . USERS_TABLE . ' SET user_type = 1 , user_inactive_reason = 5, user_inactive_time = ' . time() . ' WHERE user_id = ' . $uid;
 			$db->sql_query($sql);
 			add_log('user', $uid, 'INACTIVE_BY_USER', $d_reason, $user->data['username']);
-			
+
 			meta_refresh(3, append_sid("{$phpbb_root_path}index.$phpEx"));
 			$user->session_kill();
 			$user->session_begin();
-			trigger_error($user->lang['UCP_PROFILE_DEACTIVAT_SUCCES']);			
+			trigger_error($user->lang['UCP_PROFILE_DEACTIVAT_SUCCES']);
 		}
 
 		$template->assign_vars(array(
